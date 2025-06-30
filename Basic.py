02678a317fa1fcfb412b068c201462e64ebfc6f0
Vennela234 <chakalivennela4@gmail.com>
2025-06-30 14:40:47 +0530

    first commit

---
+++ Basic.py
@@ -0,0 +1,40 @@
+# print("hello")
+# print("hi")
+# a=10
+# b,c=12,34
+# d=e=f=23
+# print(a)
+# print(c)
+# print(e)
+# a=int(input("Enter a num : "))
+# p='7'
+
+#print(a)
+# b=int(input("Enter a num : "))
+# print(a+b)
+# d,e = map(int,input("Enter num : ").split())
+# print(d)
+# age=18
+# if(age>18):
+#     print("Not Eligible")
+# elif age==18:
+#     print("Welcome new voter")
+# else:
+#     print("Not Eligible")
+# print("if cond executed")
+
+# if(p in str(a)):
+#     print("yes")
+# else:
+#     print("No")
+# if(a>0 and a<=99):
+#      if((a%10==7) or (a//10==7)):
+#          print("yes")
+#      else:
+#          print("No")
+# print("invalid")
+# list=[10,20,True]
+# print(sum(list))
+
+l2=[x for x in range(10) if x%2==1]
+print(l2)
---
+++ README.md
@@ -0,0 +1,2 @@
+"# mypythonfile" 
+"# mypythonfile" 
